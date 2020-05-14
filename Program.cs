0a217b4bd0aaf1fc0e309249fe3bffe23eecc7ad
e.serqueira <eduardo.serqueira@outlook.com>
2020-05-13 23:21:22 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio_Um
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            var num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0 && num >= 0)
+                Console.WriteLine("Par positivo");
+            else if (num % 2 == 0 && num < 0)
+                Console.WriteLine("Par negativo");
+            else if (num % 2 != 0 && num >= 0)
+                Console.WriteLine("Impar positivo");
+            else if (num % 2 != 0 && num < 0)
+                Console.WriteLine("Impar negativo");
+
+            Console.ReadKey();
+        }
+    }
+}
